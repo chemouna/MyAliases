508c38edd89e6fe83f8c1f83ba9edb7dfedb3805
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-01-23 11:41:22 +0000

    Add aliases.

---
+++ aliases.zsh
@@ -0,0 +1,10 @@
+
+
+alias sz='source ~/.zshrc'
+alias ad='adb devices'
+alias gfu='git fetch upstream'
+alias grum='git rebase upstream/master'
+alias wadb='killall adb; adb start-server; adb devices'
+alias gpf='git push --force-with-lease'
+
+
