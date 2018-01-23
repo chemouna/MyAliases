dba952ed083a398d02091adf180b8bdbec5617e7
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-01-23 11:47:58 +0000

    Add mac zip aliases.

--- aliases.zsh
+++ aliases.zsh
@@ -6,5 +6,6 @@ alias gfu='git fetch upstream'
 alias grum='git rebase upstream/master'
 alias wadb='killall adb; adb start-server; adb devices'
 alias gpf='git push --force-with-lease'
-
+alias czwe='f(){ zip -r $1 $2 -x "*.DS_Store" -x "__MACOSX" }; f'
+alias rze='f(){ zip -d $1 __MACOSX/\* ;  zip -d $1 *.DS_Store }; f'
 
